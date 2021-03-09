535e370e34acbc2f987c9161a99db0ccb7125aef
MRSadeghi78 <mary.nzn@gmail.com>
2021-03-09 19:24:18 +0330

    Code refactored

---
+++ Phase04/Phase04/FileReader.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+
+namespace Phase04
+{
+    class FileReader
+    {
+        public static string Read(string FilePath)
+        {
+            return File.ReadAllText(FilePath);
+        }
+    }
+}
--- Phase04/Phase04/Program.cs
+++ Phase04/Phase04/Program.cs
@@ -1,7 +1,5 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 
 namespace Phase04
@@ -10,23 +8,16 @@ namespace Phase04
     {
         static void Main(string[] args)
         {
-            var studentJson = File.ReadAllText("../../../Students.json");
-            var students = JsonConvert.DeserializeObject<List<Student>>(studentJson);
-
-            var scoreJson = File.ReadAllText("../../../Scores.json");
-            var scores = JsonConvert.DeserializeObject<List<Mark>>(scoreJson);
-
-            var firstThreeStudents = students.Take(3);
-
-            Console.WriteLine("Printing 3 first students based on their position in Students.json file...");
-            foreach (Student student in firstThreeStudents)
-                Console.WriteLine(student.FirstName + " " + student.LastName + " : " + scores.Where(s => s.StudentNumber == student.StudentNumber).Average(s => s.Score));
-            Console.WriteLine();
-
-            var firstThreeAverages = students.GroupJoin(scores, st => st.StudentNumber, sc => sc.StudentNumber, (st, sc) => new { FirstName = st.FirstName, LaseName = st.LastName, Average = sc.Average(s => s.Score)}).OrderByDescending(s => s.Average).Take(3);
-            Console.WriteLine("Printing 3 first students based on their averages...");
-            foreach (var student in firstThreeAverages)
-                Console.WriteLine(student.FirstName + " " + student.LaseName + " : " + student.Average);
+            var students = Parser.jsonToList<Student>(FileReader.Read("../../../Students.json"));
+            var scores = Parser.jsonToList<Mark>(FileReader.Read("../../../Scores.json"));
+            var firstThreeAverages = FindTop3Students(students, scores);
+            Output.PrintList(firstThreeAverages);    
         }
+
+        static IEnumerable<StudentAverage> FindTop3Students(List<Student> Students, List<Mark> Scores)
+        {
+            return Students.GroupJoin(Scores, st => st.StudentNumber, sc => sc.StudentNumber, (st, sc) => new StudentAverage (st.FirstName, st.LastName,sc.Average(s => s.Score))).OrderByDescending(s => s.Average).Take(3);
+        }
+
     }
 }
